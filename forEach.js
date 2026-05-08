0358e3f8332535d933af55d3fc05bc8dfadcbc0f
Anam <itsanam777@gmail.com>
2026-05-08 21:59:57 +0600

    Add files via upload

---
+++ forEach.js
@@ -0,0 +1,11 @@
+let nums=[2,3,4,5];
+nums.forEach((num)=>
+{
+    console.log(num*num);
+})
+
+
+let cities=["Dhaka","Cumilla","Rongpur","Shirajganj"];
+cities.forEach((city,idx)=>{
+    console.log(idx,city.toUpperCase());
+})
