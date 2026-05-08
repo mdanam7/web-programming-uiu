bffe4fba0f0d8c6a8c7b607d8306fe90cffffc6c
Anam <itsanam777@gmail.com>
2026-05-08 10:34:28 +0600

    Add files via upload

---
+++ firstpractice.js
@@ -0,0 +1,24 @@
+console.log("FAHH");
+let age =21;
+const owner="Anam";//constant can't be redeclared
+console.log(age);
+let x=null;//null mane khali
+let y=undefined;// undefined mane ki ache jani na
+let following_mumtahina=true;
+console.log(following_mumtahina);
+const Bangladesh={
+    obosthan:"South Asia",
+    protibeshi_rashtro:"India and Myanmar",
+    jatio_fol:"Jackfruit",
+    jonosongkha:"168.70 mllion",
+    total_bivagh:8,
+
+};
+Bangladesh["jatio_fol"]="Kathal";
+console.log(Bangladesh.jatio_fol);
+console.log(Bangladesh["jonosongkha"]);
+let a=2;
+let b=3;
+console.log("a+b=",a+b);
+
+console.log("a**=4 means a to the power 4",a**=4);
